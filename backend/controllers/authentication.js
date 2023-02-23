7f115c802ba6121e9498ec12c5be7dd59a889e77
Tyler-Will <tylerceeme@gmail.com>
2023-02-22 21:12:30 -0800

    session authentication added

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -13,17 +13,30 @@ router.post('/', async (req, res) => {
     })
 
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ 
-            message: `Could not find a user with the provided username and password` 
+        res.status(404).json({ message: `Could not find a user with the provided username and password` 
         })
     } else {
+        req.session.userId = user.userId
         res.json({ user })
     }
-
-
-    console.log(user)
 })
   
+
+router.get('/profile', async (req, res) => {
+    try {
+       let user = await User.findOne({
+           where: {
+               userId: req.session.userId
+           }
+       })
+        res.json(user)
+    } catch {
+       res.json(null)
+    }
+})
+
+
+
 
 
 module.exports = router
